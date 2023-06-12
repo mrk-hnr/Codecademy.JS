7dca66c32482f745981f032c6bd57828c74fcb2a
mrk-hnr <mrk.hnr@hotmail.com>
2023-06-12 14:12:46 +0800

    Committing Creating an Array Exercies

---
+++ 010 - Arrays/001 - Create an Array/main.js
@@ -0,0 +1,23 @@
+// Create an Array
+
+// One way we can create an array is to use an array literal. An array literal creates an array by wrapping items in square brackets []. Remember from the previous exercise, arrays can store any data type — we can have an array that holds all the same data types or an array that holds different data types. 
+
+//             IMAGE NOT AVAILABLE
+
+// Let’s take a closer look at the syntax in the array example:
+// * The array is represented by the square brackets [] and the content inside.
+// * Each content item inside an array is called an element. 
+// * There are three different elements inside the array.
+// * Each element inside the array is a different data type. 
+
+// We can also save an array to a variable. You may have noticed we did this in the previous exercise:
+//     let newYearsResolutions = ['Keep a journal', 'Take a falconry class', 'Learn to juggle'];
+
+// Let’s practice by making an array of our own.
+
+// 1.  Declare a variable using const named hobbies and set it equal to an array with three strings inside of it.
+// 2. Use console.log() to print hobbies to the console.
+
+const hobbies = ["sleeping", "eating", "gaming"]
+
+console.log(hobbies)
